5ff8a6ffc35be07acb6a89571882b74bc451c18c
Cedric Murer <cedric.murer@lunatech.fr>
2018-10-11 15:08:28 +0200

    Update the HomePage to be a component

--- client/src/main/scala/ReactApp.scala
+++ client/src/main/scala/ReactApp.scala
@@ -1,12 +1,12 @@
+import scala.scalajs.js.annotation.JSExport
+
 import fr.lunatech.fpfpp.HomePage
 import org.scalajs.dom
-
-import scala.scalajs.js.annotation.JSExport
 
 object ReactApp {
 
   @JSExport
   def main(args: Array[String]): Unit = {
-    HomePage().mapUnmounted(_.renderIntoDOM(dom.document.getElementById("app")))
+    HomePage(HomePage.Props()).renderIntoDOM(dom.document.getElementById("app"))
   }
 }
--- client/src/main/scala/fr/lunatech/fpfpp/HomePage.scala
+++ client/src/main/scala/fr/lunatech/fpfpp/HomePage.scala
@@ -1,25 +1,36 @@
 package fr.lunatech.fpfpp
 
 import fr.lunatech.fpfpp.component.{Button, Card}
-import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
 
 object HomePage {
 
-  def apply() = ScalaComponent
-    .static("Hello")(
+  def apply(props: Props) = component(props)
+
+  val component =
+    ScalaComponent.builder[Props]("HomePage")
+      .renderBackend[Backend]
+      .build
+
+  case class Props()
+
+  class Backend($ : BackendScope[Props, Unit]) {
+
+    def render(props: Props) =
       <.div(
-        ^.className:="fpfpp",
+        ^.className := "fpfpp",
         <.div(
-          ^.className:="fpfpp-cards",
+          ^.className := "fpfpp-cards",
           Card()
         ),
         <.div(
-          ^.className:="fpfpp-buttons",
+          ^.className := "fpfpp-buttons",
           Button(Button.Props("nop", "fa-remove", Callback.empty)),
           Button(Button.Props("refresh", "fa-refresh", Callback.empty)),
           Button(Button.Props("love", "fa-heart", Callback.empty))
         )
       )
-    )
+  }
+
 }
