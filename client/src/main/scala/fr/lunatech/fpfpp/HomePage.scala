6c66bbe23efb2dae94482fa6cf39c41adcc4bbe2
Cedric Murer <cedric.murer@lunatech.fr>
2018-10-11 14:52:40 +0200

    add buttons + start to define html struct

--- client/src/main/scala/fr/lunatech/fpfpp/HomePage.scala
+++ client/src/main/scala/fr/lunatech/fpfpp/HomePage.scala
@@ -1,6 +1,6 @@
 package fr.lunatech.fpfpp
 
-import fr.lunatech.fpfpp.component.Card
+import fr.lunatech.fpfpp.component.{Button, Card}
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
@@ -9,7 +9,17 @@ object HomePage {
   def apply() = ScalaComponent
     .static("Hello")(
       <.div(
-        Card()
+        ^.className:="fpfpp",
+        <.div(
+          ^.className:="fpfpp-cards",
+          Card()
+        ),
+        <.div(
+          ^.className:="fpfpp-buttons",
+          Button(Button.Props("nop", "fa-remove", Callback.empty)),
+          Button(Button.Props("refresh", "fa-refresh", Callback.empty)),
+          Button(Button.Props("love", "fa-heart", Callback.empty))
+        )
       )
     )
 }
---
+++ client/src/main/scala/fr/lunatech/fpfpp/component/Button.scala
@@ -0,0 +1,36 @@
+package fr.lunatech.fpfpp.component
+
+import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
+import scalacss.DevDefaults._
+import scalacss.ScalaCssReact._
+import scalacss.internal.mutable.StyleSheet
+
+object Button {
+
+  val component = ScalaComponent
+    .builder[Props]("Button")
+    .renderBackend[Backend]
+    .build
+
+  def apply(props: Props) = component(props)
+
+  case class Props(name: String, icon: String, action: Callback)
+
+  class Backend($: BackendScope[Props, Unit]) {
+    def render(props: Props) =
+      <.button(Style.button, ^.id := props.name, ^.className := s"fa ${props.icon}")
+  }
+
+  object Style extends StyleSheet.Inline {
+    import dsl._
+    val button = style(
+      fontSize(32.px),
+      verticalAlign.middle,
+      padding(0.px),
+      margin(0.px)
+    )
+  }
+
+  Style.addToDocument()
+}
